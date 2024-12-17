c8f886c00835dc2d6a15a86736c6149b67d99e0d
Rezo Joglidze <rezojoglidze7@gmail.com>
2024-12-17 18:43:36 +0400

    Initial Commit

---
+++ weather-tracker/weather_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_trackerApp.swift
+//  weather-tracker
+//
+//  Created by Rezo Joglidze on 17.12.24.
+//
+
+import SwiftUI
+
+@main
+struct weather_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
